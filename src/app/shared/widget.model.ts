3328a870226148c846ab3d2796930ae974e46b32
Connie <cleung334@gmail.com>
2017-10-17 08:08:29 +0800

    Solution to challenge 4.  I overanalyzes the question and did it in a complicated way

--- src/app/shared/widget.model.ts
+++ src/app/shared/widget.model.ts
@@ -10,18 +10,21 @@ export const widgets = [
   {
     "id": 1,
     "name": "Widget 1",
+    'img': '/assets/img/number-1.png',
     "description": "This is a description",
     "featured": true
   },
   {
     "id": 2,
     "name": "Widget 2",
+    'img': '/assets/img/number-2-blue-icon.png',
     "description": "This is a description!",
     "featured": false
   },
   {
     "id": 3,
     "name": "Widget 3",
+    'img': '/assets/img/number-3-icon.png',
     "description": "This is a lovely widget",
     "featured": false
   }
