449e27ab4d8aeaab9ec0cfe6c32d3c7f13b0572e
Connie <cleung334@gmail.com>
2017-10-22 22:38:25 +0800

    implement widget-details component

--- src/app/widgets/widget-details/widget-details.component.ts
+++ src/app/widgets/widget-details/widget-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Widget } from '../../shared';
 
 @Component({
   selector: 'app-widiget-details',
@@ -8,7 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WidgetDetailsComponent implements OnInit {
 
-  
+  @Input()
+  selectedWidget: Widget;
+
   constructor() {
 
   }
